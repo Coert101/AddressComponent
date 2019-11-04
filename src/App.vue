6bdcd32bea3a1005b3afbd8322453c53521278c8
CoertDM <coert@dotmodus.com>
2019-11-04 14:14:15 +0200

    Mapping the resultant address to a n output on UI.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-    </div>
     <router-view/>
   </div>
 </template>
@@ -14,6 +11,11 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
 }
 
 #nav {
