377fc22ab8a4249cebc5c468d0f9c9f0bc4ebc6c
CoertDM <coert@dotmodus.com>
2019-11-04 13:27:55 +0200

    Implementing the autocomplete.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <address-auto-fill></address-auto-fill>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AddressAutoFill from '../components/address-component/AddressAutoFill.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    AddressAutoFill
   }
 }
 </script>
